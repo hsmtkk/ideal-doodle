685ee9c11d27973458badaa19a807f8d75b6f695
foo <foo@bar.com>
2021-12-06 13:09:23 +0900

    impl hello world web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let app_name = std::env::var("HEROKU_APP_NAME").expect("HEROKU_APP_NAME environment variable");
+    let port = std::env::var("PORT").expect("PORT environment variable");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL environment variable");
+    let bind_address = format!("0.0.0.0:{}", port);
+
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&bind_address.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
